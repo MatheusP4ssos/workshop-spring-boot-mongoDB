ff5d615c19c287bb03c023a56a30a1414eec052b
Matheus Passos <matheus.senai2016@gmail.com>
2025-07-11 01:37:03 -0300

    Initial commit

---
+++ workshopMongo/src/test/java/com/matheusHolanda/workshopMongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusHolanda.workshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
