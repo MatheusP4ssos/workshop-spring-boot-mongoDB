382bfa8f2a08e69b59fcbee5b94f5c20f478c165
Matheus Passos <matheus.senai2016@gmail.com>
2025-07-15 03:20:08 -0300

    Delecao de usuario com DELETE

--- workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/resources/UserResource.java
+++ workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.matheusHolanda.workshopMongo.dto.UserDTO;
 import com.matheusHolanda.workshopMongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,9 +27,23 @@ public class UserResource {
         return ResponseEntity.ok().body(listDto);   // Retorna a lista como resposta HTTP com status 200 OK
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) { // Método que retorna uma resposta HTTP contendo uma lista de usuários
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));   // Retorna a lista como resposta
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<UserDTO> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/services/UserService.java
+++ workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.matheusHolanda.workshopMongo.services;
 
 import com.matheusHolanda.workshopMongo.domain.User;
+import com.matheusHolanda.workshopMongo.dto.UserDTO;
 import com.matheusHolanda.workshopMongo.repository.UserRepository;
 import com.matheusHolanda.workshopMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,18 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
-}
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+
+        public User fromDTO (UserDTO objDto){
+            return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+        }
+    }
