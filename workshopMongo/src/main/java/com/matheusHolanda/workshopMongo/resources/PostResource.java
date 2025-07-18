5b4c5e736bc330e2bd29977d3b86fbea49dec0ba
Matheus Passos <matheus.senai2016@gmail.com>
2025-07-18 01:33:54 -0300

    Consulta simples com querry methods

--- workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/resources/PostResource.java
+++ workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.matheusHolanda.workshopMongo.resources;
 import com.matheusHolanda.workshopMongo.domain.User;
 import com.matheusHolanda.workshopMongo.dto.UserDTO;
 import com.matheusHolanda.workshopMongo.entities.Post;
+import com.matheusHolanda.workshopMongo.resources.util.URL;
 import com.matheusHolanda.workshopMongo.services.PostService;
 import com.matheusHolanda.workshopMongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,5 +28,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) { // Método que retorna uma resposta HTTP contendo uma lista de posts
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);   // Retorna a lista como resposta
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitleContaining(text);
+        return ResponseEntity.ok().body(list);   // Retorna a lista como resposta
     }
 }
---
+++ workshopMongo/src/main/java/com/matheusHolanda/workshopMongo/resources/util/URL.java
@@ -0,0 +1,12 @@
+package com.matheusHolanda.workshopMongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+    public static String decodeParam(String text)
+            throws UnsupportedEncodingException {
+        return URLDecoder.decode(text, "UTF-8");
+    }
+}
